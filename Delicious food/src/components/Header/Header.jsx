14c14c229a224d2200026aa798420aac1872d9fc
Simran_Kodwani <kodwanisimran03@gmail.com>
2025-01-13 17:25:01 +0530

    Delicious foods

---
+++ Delicious food/src/components/Header/Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+    return (
+        <div className='header'>
+            <div className='header-contents'>
+                <h2>Bring Your Favorite Meals Home!</h2>
+                <p>Discover the joy of effortless dining with just a few taps. Whether it's breakfast,
+                     lunch, or dinner, we’ve got your cravings coveredDiscover the joy of effortless 
+                     dining with just a few taps. Whether it's breakfast, lunch, or dinner, we’ve got 
+                     your cravings covered</p>
+                <button>View Menu</button>
+            </div>
+        </div>
+    )
+}
+
+export default Header
